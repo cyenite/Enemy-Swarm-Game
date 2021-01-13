fe626ebb8c15c112e21f3e1a7cbad6535415c8e2
Akron254 <aaronrono42@gmail.com>
2021-01-13 19:29:17 +0300

    Added gameController

--- lib/game_controller.dart
+++ lib/game_controller.dart
@@ -2,15 +2,24 @@ import 'package:flame/flame.dart';
 import 'package:flame/game/game.dart';
 import 'package:flutter/material.dart';
 
+import 'components/player.dart';
+
 class GameController extends Game {
   Size screenSize;
   double tileSize;
+  Player player;
 
   GameController() {
     initialize();
   }
 
-  void render(Canvas c) {}
+  void render(Canvas c) {
+    Rect background = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint backgroundPaint = Paint()..color = Color(0xFFFAFAFA);
+    c.drawRect(background, backgroundPaint);
+
+    player.render(c)
+  }
 
   void update(double t) {}
 
@@ -21,5 +30,6 @@ class GameController extends Game {
 
   Future<void> initialize() async {
     resize(await Flame.util.initialDimensions());
+    player = Player(this);
   }
 }
